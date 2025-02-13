d781ddbee985ed7fffa63745de0326ff73841aa2
Abhilasha <singhabhilasha4042@gmail.com>
2025-02-13 22:17:18 +0530

    updates manager agent to just show activities

--- agents/manage_agent.py
+++ agents/manage_agent.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from Azent.Azent import Agent
-from tools.get_activities_tool import get_activities_by_activity_name, get_activities_by_group_type_or_travel_theme
+from tools.get_activities_tool import get_activities_by_activity_name, get_activities_by_group_type_or_travel_theme_and_number_of_days
 from tools.get_hotels_tool import get_hotels_by_destination
 from tools.itinerary_tool import ItineraryTool
 from opik import track
@@ -27,14 +27,12 @@ class ManagerAgent:
                 - Ask very specific question like where you want to go something like that.
                 - manager greets the user and ask for the name to make conversation more personalized. Don't ask name if user already provided it.
                 - manager asks for the user's preferences like destination, travel_theme, group_type, number_of_days, ask user once question at a time, don't ask multiple questions at once.
-                - when user gives both travel_theme and group_type information, then call <get_activities_by_group_type_or_travel_theme> tool to get the activities for the trip and ask user to choose the activities.
-                - once user selects activities, then call <get_hotels_by_location> tool to show the hotels for the trip and ask user to choose the hotels.
+                - when user gives travel_theme, group_type information and number_of_days info, then call <get_activities_by_group_type_or_travel_theme_and_number_of_days> tool to get the activities for the trip and just show the activities to the user.
+                - Once the acitivities is fetched, then call <get_hotels_by_location> tool to show the hotels for the trip and ask user to choose only one hotel. Don't ask user preferences for hotel like preference for accommodation.
                 - once manager has gathered all the information from the user, then call <get_base_itinerary> tool to get the base itinerary for the trip and ask user to confirm the itinerary.
                 - manager asks the user for any changes to the itinerary if user wants to change anything.
                 - manager can call <get_activities_by_activity_name> tool to get the activities for the trip if user wants to add new activities
                 - manager can call <get_hotels_by_location> tool to get the hotels for the trip if user wants to add new hotels
-                - After, the base itinerary has been created, user may want to change the number_of_days, if days needs to increased, you should ask user, shall I fetch more activities. And, if user want to decrease the number of days, then you should ask user, shall I remove some activities.
-                - After, the base itinerary has been created, user may want to change the activities, if user wants to update the activities, fetch the activities again to show to user using tool get_activities_by_group_type_or_travel_theme
 
             User Specific Information>:
                 session_id: {session_id}
@@ -45,7 +43,7 @@ class ManagerAgent:
 
             Available Tools:
                 - <get_base_itinerary> : call this tool only when user has confirmed all the options and you have information about activities and hotels.
-                - <get_activities_by_group_type_or_travel_theme> : when user has provided you group_type and travel_theme, then call this tool to fetch relevant activities and ask user to choose from it.
+                - <get_activities_by_group_type_or_travel_theme_and_number_of_days> : when user has provided you group_type, travel_theme and number_of_days, then call this tool to fetch relevant activities and ask user to choose from it.
                 - <get_hotels_by_destination> : get hotel details by group_type, travel_theme  and/or with hotel description (ex: "luxury 5-star hotel with spa")
                 - <finalize_itinerary> : If user has pressed confirm button, then use tool finalize_itinerary to finalize the itinerary.
                 - <get_activities_by_activity_name> : If user explicit ask for activity details like "I want to go to the beach", then use tool get_activities_by_activity_name
@@ -63,7 +61,7 @@ class ManagerAgent:
             tools=[
                 ItineraryTool().get_base_itinerary,
                 get_activities_by_activity_name,
-                get_activities_by_group_type_or_travel_theme, 
+                get_activities_by_group_type_or_travel_theme_and_number_of_days, 
                 get_hotels_by_destination
             ],
         )
