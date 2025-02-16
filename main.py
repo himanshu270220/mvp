b93fb4f50b5d391fcba8a0b1393ef9e75468f1ea
Abhilasha <singhabhilasha4042@gmail.com>
2025-02-16 11:44:59 +0530

    removes debug

--- main.py
+++ main.py
@@ -144,4 +144,4 @@ def handle_chat():
 
 if __name__ == '__main__':
     logger.info("Starting Flask application.")
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001)
