73e6929dab7a410e264b301ce95d60ff29e7507e
Abhilasha <singhabhilasha4042@gmail.com>
2025-02-19 22:04:58 +0530

    updates itinerary editor prompt

--- prompts/get_itinerary_editor_prompt.py
+++ prompts/get_itinerary_editor_prompt.py
@@ -8,11 +8,20 @@ def get_itinerary_editor_prompt(package):
     There will be mainly two updates that user wants to update:
     
     1. Hotel Update flow:
-    The overall workflow for hotels update can be described as follows:
-        - User comes to the you
-        - User will ask very specific question like "please add 4 stars hotel to my itinerary" or "please add hotel which is beachside"
-        - You need to first check the hotels whether these hotel exists in our database by calling tool <get_hotels>
-        - call <update_itinerary> to update the existing itinerary
+        The overall workflow for hotels update can be described as follows:
+            - User comes to the you
+            - User will ask very specific question like "please add 4 stars hotel to my itinerary" or "please add hotel which is beachside"
+            - You need to first check the hotels whether these hotel exists in our database by calling tool <get_hotels>
+            - call <update_itinerary> to update the existing itinerary
+
+    2. Activity Update flow:
+        The overall workflow for activities update can be described as follows:
+            - User comes to the you
+            - User may ask to remove an activity from the itinerary, in that case just call <update_itinerary> to update the existing itinerary
+            - User may ask very specific question like "please add skyscrapping activities to my itinerary" or "please add activities which are near the burj khalifa"
+            - User may ask to add an activity to the itinerary, user may ask like "please add skyscrapping activities", in that case just call <get_activities_by_activity_name> to get the activities
+            - If user ask to remove any activity and add new activity at the same time, first call <get_activities_by_activity_name> to get the activities, and ask user to confirm the changes.
+            - call <update_itinerary> to update the existing itinerary
    
     User Specific Information:
         current_user_itinerary: {package}
@@ -24,6 +33,7 @@ def get_itinerary_editor_prompt(package):
     Available Tools:
         - <get_hotels> : get hotel details by destination, group_type or travel_theme or star_rating (ex: "please add 4 stars hotel") or with hotel description (ex: "luxury 5-star hotel with spa")
         - <update_itinerary> : update itinerary for user changes once user confirms all the changes
+        - <get_activities_by_activity_name> : get activities by activity name (ex: "please add skyscrapping activities")
     
     Important Points to Follow:
      - If user says keep anything thing for example: "keep Taj Luxe Central at Central District, Dubai activity" then don't ask for confirmation and directly call <update_itinerary> to update the existing itinerary
